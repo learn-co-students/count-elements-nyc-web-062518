cca90350d9884ea4d8a66e73a1dc2dfba448020e
github username <github email address>
2018-07-02 14:07:27 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
+animals = ['tortoise', 'aye-aye', 'honey badger', 'aye-aye', 'tortoise', 'tortoise']
+
 def count_elements(array)
   # code goes here
+  count = Hash.new(0)
+  array.each do |item|
+    count[item] +=1
+  end
+    count 
+
 end
- 
